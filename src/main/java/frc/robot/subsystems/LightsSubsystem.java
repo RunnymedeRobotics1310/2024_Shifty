b12bb35e21ba61c66034d2f808c6ceb468cf3f94
Runnymede Driverstation <1310.runnymede.robotics@gmail.com>
2024-01-15 17:15:35 -0500

    update colour light commands

--- src/main/java/frc/robot/subsystems/LightsSubsystem.java
+++ src/main/java/frc/robot/subsystems/LightsSubsystem.java
@@ -11,6 +11,7 @@ public class LightsSubsystem extends SubsystemBase {
     private final AddressableLED       ledStrip;
     private final AddressableLEDBuffer ledBuffer;
     private int                        rainbowFirstPixelHue = 0;
+    private int                        firstPixelValue      = 0;
 
     /** Creates a new DriveSubsystem. */
     public LightsSubsystem() {
@@ -49,20 +50,176 @@ public class LightsSubsystem extends SubsystemBase {
         // For every pixel
         for (var i = 0; i < ledBuffer.getLength(); i++) {
 
-            // Calculate the hue - hue is easier for rainbows because the color
-            // shape is a circle so only one value needs to precess
-            final var hue = 5;
-
             // Set the value
-            ledBuffer.setHSV(i, hue, 255, 15);
+            ledBuffer.setHSV(i, 5, 255, 15);
         }
 
+    }
+
+    private void updateRed() {
+
+        // For every pixel
+        for (var i = 0; i < ledBuffer.getLength(); i++) {
+
+            // Calculate the value - I want the orange to move
+            // shape is a circle so only one value needs to precess
+            final var value = (firstPixelValue + (i * 180 / ledBuffer.getLength())) % 180;
+
+            // Set the value
+            ledBuffer.setHSV(i, 0, 255, value);
+        }
+
+        // Increase by to make the rainbow "move"
+        firstPixelValue += 2;
+
+        // check bounds
+        firstPixelValue %= 180;
+    }
+
+    private void updateOrange() {
+
+        // For every pixel
+        for (var i = 0; i < ledBuffer.getLength(); i++) {
+
+            // Calculate the value - I want the orange to move
+            // shape is a circle so only one value needs to precess
+            final var value = (firstPixelValue + (i * 180 / ledBuffer.getLength())) % 180;
+
+            // Set the value
+            ledBuffer.setHSV(i, 2, 255, value);
+        }
+
+        // Increase by to make the rainbow "move"
+        firstPixelValue += 2;
+
+        // check bounds
+        firstPixelValue %= 180;
+    }
+
+    private void updateYellow() {
+
+        // For every pixel
+        for (var i = 0; i < ledBuffer.getLength(); i++) {
+
+            // Calculate the value - I want the orange to move
+            // shape is a circle so only one value needs to precess
+            final var value = (firstPixelValue + (i * 180 / ledBuffer.getLength())) % 180;
+
+            // Set the value
+            ledBuffer.setHSV(i, 10, 255, value);
+        }
+
+        // Increase by to make the rainbow "move"
+        firstPixelValue += 2;
+
+        // check bounds
+        firstPixelValue %= 180;
+    }
+
+    private void updateGreen() {
+
+        // For every pixel
+        for (var i = 0; i < ledBuffer.getLength(); i++) {
+
+            // Calculate the value - I want the orange to move
+            // shape is a circle so only one value needs to precess
+            final var value = (firstPixelValue + (i * 180 / ledBuffer.getLength())) % 180;
+
+            // Set the value
+            ledBuffer.setHSV(i, 50, 255, value);
+        }
+
+        // Increase by to make the rainbow "move"
+        firstPixelValue += 2;
+
+        // check bounds
+        firstPixelValue %= 180;
+    }
+
+    private void updateBlue() {
+
+        // For every pixel
+        for (var i = 0; i < ledBuffer.getLength(); i++) {
+
+            // Calculate the value - I want the orange to move
+            // shape is a circle so only one value needs to precess
+            final var value = (firstPixelValue + (i * 180 / ledBuffer.getLength())) % 180;
+
+            // Set the value
+            ledBuffer.setHSV(i, 300, 255, value);
+        }
+
+        // Increase by to make the rainbow "move"
+        firstPixelValue += 2;
+
+        // check bounds
+        firstPixelValue %= 180;
+    }
+
+    private void updatePurple() {
+
+        // For every pixel
+        for (var i = 0; i < ledBuffer.getLength(); i++) {
+
+            // Calculate the value - I want the orange to move
+            // shape is a circle so only one value needs to precess
+            final var value = (firstPixelValue + (i * 180 / ledBuffer.getLength())) % 180;
+
+            // Set the value
+            ledBuffer.setHSV(i, 315, 255, value);
+        }
+
+        // Increase by to make the rainbow "move"
+        firstPixelValue += 2;
+
+        // check bounds
+        firstPixelValue %= 180;
+    }
+
+    private void updatePink() {
+
+        // For every pixel
+        for (var i = 0; i < ledBuffer.getLength(); i++) {
+
+            // Calculate the value - I want the orange to move
+            // shape is a circle so only one value needs to precess
+            final var value = (firstPixelValue + (i * 180 / ledBuffer.getLength())) % 180;
+
+            // Set the value
+            ledBuffer.setHSV(i, 359, 255, value);
+        }
+
+        // Increase by to make the rainbow "move"
+        firstPixelValue += 2;
+
+        // check bounds
+        firstPixelValue %= 180;
+    }
+
+    private void endGame() {
+
+        // For every pixel
+        for (var i = 0; i < ledBuffer.getLength(); i++) {
+
+            // Calculate the value - I want the orange to move
+            // shape is a circle so only one value needs to precess
+            final var value = (firstPixelValue + (i * 180 / ledBuffer.getLength())) % 180;
+
+            // Set the value
+            ledBuffer.setHSV(i, 0, 255, value);
+        }
+
+        // Increase by to make the rainbow "move"
+        firstPixelValue -= 10;
+
+        // check bounds
+        firstPixelValue %= 180;
     }
 
     @Override
     public void periodic() {
 
-        orange();
+        updateOrange();
         ledStrip.setData(ledBuffer);
 
         // SmartDashboard.putNumber("Hue", rainbowFirstPixelHue);
