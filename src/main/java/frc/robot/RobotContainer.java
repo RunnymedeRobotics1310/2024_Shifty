e4469cb89a17ac9f741b494e048a3c8d6b77ac57
Jeff Zakrzewski <jeff@zakr.ca>
2024-02-03 13:29:13 -0500

    Add basic VisionSubsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,6 +19,7 @@ import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LightsSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -35,6 +36,7 @@ public class RobotContainer {
     // The robot's subsystems and commands are defined here...
     private final DriveSubsystem   driveSubsystem   = new DriveSubsystem();
     private final LightsSubsystem  lightsSubsystem  = new LightsSubsystem();
+    private final VisionSubsystem  visionSubsystem  = new VisionSubsystem();
     private final IntakeSubsystem  IntakeSubsystem  = new IntakeSubsystem(lightsSubsystem);
     private final ShooterSubsystem ShooterSubsystem = new ShooterSubsystem(lightsSubsystem);
 
